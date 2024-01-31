7cafb36d8b37bc87f7ed5ba23467a0070c3dae9c
Dimitrije Stojilkovic <dimitrije.stojilkovic@elfak.rs>
2024-01-31 14:02:57 +0100

    rodja

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,9 +1,31 @@
 using Microsoft.AspNetCore.SignalR;
-
+using backend.DTOs;
 namespace backend.Hubs;
 
 public class ChatHub : Hub{
-    public async Task SendMessage(string user, string message){
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
-    }
+    public readonly string _botUser;
+        private readonly IDictionary<string, UserConnection> _connections;
+
+        public ChatHub(IDictionary<string, UserConnection> connections)
+        {
+            _botUser = "MyChat bot";
+            _connections = connections;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection connection))
+            {
+                await Clients.Group(connection.Room).SendAsync("ReceiveMessage", connection.User, message);
+            }
+        }
+
+        public async Task JoinRoom(string user, string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            _connections[Context.ConnectionId] = new UserConnection(user, room);
+            //await Clients.All.SendAsync("ReceiveMessage", user, "Hey everyone");
+        }
+
+
 }
